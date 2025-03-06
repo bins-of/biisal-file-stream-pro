f626344fa12f44cd8cdd0f440e954dbcaa3ac236
bins-of <binsofbins1@gmail.com>
2025-03-06 20:39:17 +0600

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,8 +18,8 @@ class Var(object):
     name = str(getenv('name', 'file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002420046638'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002316472437'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002409391347'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002409391347'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
